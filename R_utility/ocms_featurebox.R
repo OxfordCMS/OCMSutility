37e13ccf8d73b61d1fa0f82e86532de9f3fcb547
nilott <nicholas.ilott@kennedy.ox.ac.uk>
2020-09-30 00:53:15 +0100

    faceting mulitple features added

--- R_utility/ocms_featurebox.R
+++ R_utility/ocms_featurebox.R
@@ -1,12 +1,14 @@
 #' ocms_featurebox
-#' boxplot expression/abundance values of a feature of interest from a matrix
+#' boxplot expression/abundance values of a feature(s) of interest from a matrix
 #'
 #' @param abundance_matrix dataframe; samples are columns and features are rows
 #' @param metadata dataframe; samples are rows and rownames should match those in abundance_matrix
-#' @param feature character; feature to plot i.e. a feature in rownames of abundance_matrix
-#' @param group_by character; grouping variable for boxplot. Must be present in metadata
+#' @param features vector; character vector of features to plot i.e. a feature(s) in rownames of abundance_matrix
+#' @param group_by character; grouping variable for boxplot. If set then variable must be present in metadata.
+#' If NULL each sample is plotted separately.
 #' @import ggplot2
 #' @import reshape
+#' @import gtools
 #' @export
 #' @return grob
 #' @examples
@@ -14,23 +16,25 @@
 #' colnames(abundance_matrix) <- LETTERS[1:20]
 #' metadata <- data.frame(group = c(rep("group1", 5), rep("group2", 5), rep("group3", 5), rep("group4", 5)))
 #' rownames(metadata) <- LETTERS[1:20]
-#' ocms_featurebox(abundance_matrix, metadata=metadata, feature="10", group_by="group")
+#' ocms_featurebox(abundance_matrix, metadata=metadata, features="10", group_by="group")
 #'
 #' # multiple features
-#' grobs.list <- lapply(rownames(abundance_matrix)[1:4], function(x) ocms_featurebox(abundance_matrix, metadata=metadata, feature=x, group_by="group"))
-#' gridExtra::grid.arrange(grobs=grobs.list, ncol=2)
+#' ocms_featurebox(abundance_matrix, metadata=metadata, features=c("10", "20", "30", "40"))
 
-ocms_featurebox <- function(abundance_matrix, metadata=NULL, feature=NULL, group_by=NULL){
+ocms_featurebox <- function(abundance_matrix, metadata=NULL, features=NULL, group_by=NULL){
 
     # must have a feature specified
-    if (is.null(feature)){
-        stop("must specify a feature to plot")
+    if (is.null(features)){
+        stop("must specify features to plot")
     }else{
-        if (!(feature %in% rownames(abundance_matrix))){
-	    stop("feature not present in abundance_matrix - check rownames")
+        # check features are in the data
+	for (feature in features){
+	    if (!(feature %in% rownames(abundance_matrix))){
+	        stop(paste0("feature ", feature, " not found in abundance_table"))
+            }
 	}
     }
-
+    
     # check if the group_by variable is NULL
     if (is.null(group_by)){
         warning("no group_by variable specified...will plot each sample separately")
@@ -48,9 +52,9 @@ ocms_featurebox <- function(abundance_matrix, metadata=NULL, feature=NULL, group
         stop("samples between abundance_matrix and metadata do not match - do they need sorting?")
     }
 
-    # add the test_id as variable
-    mat <- abundance_matrix[feature,]
-    mat$test_id <- rownames(mat)
+    # add the feature as variable
+    mat <- abundance_matrix[features,]
+    mat$feature <- rownames(mat)
 
     # reshape
     mat.m <- reshape::melt(mat)
@@ -58,16 +62,20 @@ ocms_featurebox <- function(abundance_matrix, metadata=NULL, feature=NULL, group
     # add metadata
     mat.m$covariate <- metadata[mat.m$variable, group_by]
 
-    colours <- rainbow(length(unique(metadata[,group_by])), s=0.7, v=0.6)
+    # add enough colours - not expecting each sample to be plotted separately so shouldn't
+    # expect more than this
+    colours <- ocms_palette(n=length(unique(metadata[,group_by])), palette=c("Set2", "Set3", "Set4"))
 
     # plotting
-    featureplot <- ggplot2::ggplot(mat.m, aes(x=factor(covariate, levels=mixedsort(unique(mat.m$covariate))), y=value, colour=covariate)) +
+    featureplot <- ggplot2::ggplot(mat.m, aes(x=factor(covariate, levels=gtools::mixedsort(unique(mat.m$covariate))), y=value, colour=covariate)) +
                             geom_boxplot(outlier.alpha=0) +
                             geom_jitter(height=0, width=0.15) +
                             theme_bw() +
-                            ggtitle(feature) +
                             ylab("Abundance") +
-                            scale_colour_manual(values=colours) + xlab("") + theme(axis.text.x=element_text(angle=90))
+                            scale_colour_manual(values=colours) +
+			    xlab("") +
+			    theme(axis.text.x=element_text(angle=90)) +
+			    facet_wrap(~feature)
     return(featureplot)
 }
 
